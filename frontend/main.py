e748639004cf488497a28b4891b534cf31a317c8
kaan_devoteam <kaan.kizilirmak@devoteam.com>
2022-12-07 19:13:55 +0100

    Frontend authentication

---
+++ frontend/main.py
@@ -0,0 +1,52 @@
+import datetime
+
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+
+from flask import Flask, render_template, request
+from google.auth.transport import requests
+import google.oauth2.id_token
+
+firebase_request_adapter = requests.Request()
+@app.route('/')
+def root():
+    # Verify Firebase auth.
+    id_token = request.cookies.get("token")
+    error_message = None
+    claims = None
+
+    if id_token:
+        try:
+            # Verify the token against the Firebase Auth API. This example
+            # verifies the token on each page load. For improved performance,
+            # some applications may wish to cache results in an encrypted
+            # session store (see for instance
+            # http://flask.pocoo.org/docs/1.0/quickstart/#sessions).
+            claims = google.oauth2.id_token.verify_firebase_token(
+                id_token, firebase_request_adapter, "project-id")
+        except ValueError as exc:
+            # This will be raised if the token is expired or any other
+            # verification checks fail.
+            error_message = str(exc)
+
+        # Record and fetch the recent times a logged-in user has accessed
+        # the site. This is currently shared amongst all users, but will be
+        # individualized in a following step.
+
+    return render_template(
+        'index.html',
+        user_data=claims, error_message=error_message)
+
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    # Flask's development server will automatically serve static files in
+    # the "static" directory. See:
+    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
+    # App Engine itself will serve those files as configured in app.yaml.
+    app.run(host='127.0.0.1', port=8088, debug=True)
+
